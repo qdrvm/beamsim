562f07dfd946e457226d131287fbc5603f307eb6
turuslan <turuslan.devbox@gmail.com>
2025-06-24 10:23:05 +0500

    update example.yaml

--- src/beamsim/consts.hpp
+++ src/beamsim/consts.hpp
@@ -9,7 +9,7 @@ namespace beamsim {
     MessageSize signature_size = 1536;
     MessageSize snark_size = 131072;
     double snark1_threshold = 1;
-    double snark2_threshold = 2.0 / 3;
+    double snark2_threshold = 0.66;
     double aggregation_rate_per_sec = 40;
     double snark_recursion_aggregation_rate_per_sec = 40;
     Time pq_signature_verification_time = std::chrono::milliseconds{1};
