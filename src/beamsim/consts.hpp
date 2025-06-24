7c5486c53dc14f84aff1f71c8d6de2247b62b623
turuslan <turuslan.devbox@gmail.com>
2025-06-24 11:18:58 +0500

    config values from spec

--- src/beamsim/consts.hpp
+++ src/beamsim/consts.hpp
@@ -6,14 +6,14 @@
 namespace beamsim {
   struct Consts {
     Time signature_time = std::chrono::milliseconds{20};
-    MessageSize signature_size = 1536;
+    MessageSize signature_size = 3072;
     MessageSize snark_size = 131072;
-    double snark1_threshold = 1;
+    double snark1_threshold = 0.9;
     double snark2_threshold = 0.66;
-    double aggregation_rate_per_sec = 40;
-    double snark_recursion_aggregation_rate_per_sec = 40;
-    Time pq_signature_verification_time = std::chrono::milliseconds{1};
-    Time snark_proof_verification_time = std::chrono::milliseconds{10};
+    double aggregation_rate_per_sec = 1000;
+    double snark_recursion_aggregation_rate_per_sec = 100;
+    Time pq_signature_verification_time = std::chrono::milliseconds{50};
+    Time snark_proof_verification_time = std::chrono::milliseconds{50};
   };
 
   inline Consts &consts() {
