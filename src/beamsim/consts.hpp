d118537c7e3b159b95902b72409358ae2757be55
kamilsa <kamilsa16@gmail.com>
2025-06-27 14:49:55 +0500

    Reduce verification times for PQ signature and SNARK proof in consts.hpp

--- src/beamsim/consts.hpp
+++ src/beamsim/consts.hpp
@@ -12,8 +12,8 @@ namespace beamsim {
     double snark2_threshold = 0.66;
     double aggregation_rate_per_sec = 1000;
     double snark_recursion_aggregation_rate_per_sec = 100;
-    Time pq_signature_verification_time = std::chrono::milliseconds{50};
-    Time snark_proof_verification_time = std::chrono::milliseconds{50};
+    Time pq_signature_verification_time = std::chrono::milliseconds{3};
+    Time snark_proof_verification_time = std::chrono::milliseconds{10};
   };
 
   inline Consts &consts() {
